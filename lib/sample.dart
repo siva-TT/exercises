20364ab20122501988f385d38059f2fbbdf8bd42
siva-TT <sivakumar@touchtier.com>
2023-05-10 07:56:22 +0530

    SpaceAge

--- lib/sample.dart
+++ lib/sample.dart
@@ -188,18 +188,21 @@ bob(String greet) {
 }
 
 // #14. Space Age-------------------------
-
 class SpaceAge {
-  var seconds = 31557600;
-  var ageInSec = {
+  static const _secondInEarth = 31557699;
+  static const _orbitalPeriods = {
     'mercury': 0.2408467,
     'Venus': 0.61519726,
-    'Earth': 1.0,
+    'Earth': 1,
     'Mars': 1.8808158,
     'Jupiter': 11.862615,
     'Saturn': 29.447498,
     'Uranus': 84.016846,
     'Neptune': 164.79132
   };
-  age() {}
+  age({required String planet, required int seconds}) {
+    final age =
+        seconds / (_secondInEarth * (_orbitalPeriods[planet] as double));
+    return double.parse(age.toStringAsFixed(2));
+  }
 }
